6f0e8663cff60aa957ef66c9c20d74c94db27db9
apostolis anastasiou <apostolis.anastasiou.alpha@gmail.com>
2020-10-11 16:50:47 +0300

    Add goroutines = free resource

---
+++ go-routines-is-free-resource/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+// The point to prove is that go routines should be treated as a free resource
+// The amount of memory the consume, even when there are thousands of them, is minimal
+func main() {
+	memConsumed := func() uint64 {
+		runtime.GC()
+		var s runtime.MemStats
+		runtime.ReadMemStats(&s)
+		return s.Sys
+	}
+
+	var c <-chan interface{}
+	var wg sync.WaitGroup
+
+	// Go routine that won't exit until the process is finished
+	noop := func() { wg.Done(); <-c }
+
+	// Number of goroutines to create
+	const numGoroutines = 1e4
+	wg.Add(numGoroutines)
+
+	// Measure the amount of memory consumed before creating our goroutines
+	before := memConsumed()
+	for i := numGoroutines; i > 0; i-- {
+		go noop()
+	}
+	wg.Wait()
+
+	// Amount of consumed memory after creating the goroutines
+	after := memConsumed()
+	fmt.Printf("%.3fkb\n", float64(after-before)/numGoroutines/1000)
+}
