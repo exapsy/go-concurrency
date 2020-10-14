f50366519fa663d7282d820b1a440e573e38ee5d
exapsy <apostolis.anastasiou.alpha@gmail.com>
2020-10-14 15:21:36 +0300

    Add bridge

---
+++ patterns/bridge/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Bridge combines a channel of channels into one stream
+func main() {
+
+	orDone := func(done <-chan interface{}, c <-chan interface{}) <-chan interface{} {
+		valStream := make(chan interface{})
+
+		go func() {
+			defer close(valStream)
+			for {
+				select {
+				case <-done:
+					return
+				case v, ok := <-c:
+					if !ok {
+						return
+					}
+					select {
+					case valStream <- v:
+					case <-done:
+					}
+				}
+			}
+		}()
+
+		return valStream
+	}
+
+	bridge := func(done <-chan interface{}, chanStream <-chan <-chan interface{}) <-chan interface{} {
+		valStream := make(chan interface{})
+
+		go func() {
+			defer close(valStream)
+			for {
+				var stream <-chan interface{}
+				select {
+				case maybeStream, ok := <-chanStream:
+					if ok == false {
+						return
+					}
+					stream = maybeStream
+				case <-done:
+					return
+				}
+				for val := range orDone(done, stream) {
+					select {
+					case valStream <- val:
+					case <-done:
+					}
+				}
+			}
+		}()
+
+		return valStream
+	}
+
+	genVals := func() <-chan <-chan interface{} {
+		chanStream := make(chan (<-chan interface{}))
+		go func() {
+			defer close(chanStream)
+			for i := 0; i < 10; i++ {
+				stream := make(chan interface{}, 1)
+				stream <- i
+				close(stream)
+				chanStream <- stream
+			}
+		}()
+		return chanStream
+	}
+
+	done := make(chan interface{})
+	defer close(done)
+
+	for v := range bridge(nil, genVals()) {
+		fmt.Printf("%v\n", v)
+	}
+}
